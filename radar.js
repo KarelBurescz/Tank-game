71587f640efc02967bdd1ffbdd31ee9a4129c1bd
KarelBurescz <karel.bures.prg@gmail.com>
2024-01-05 20:42:14 +0100

    Radar and true coordinates of players

--- radar.js
+++ radar.js
@@ -1,70 +1,128 @@
 import { Solider } from "./solider.js";
 import { UiObject } from "./uiobject.js";
-
+import { Game } from "./game.js";
 class Radar {
-    constructor(diameter) {
-        this.diameter = diameter;
-        this.phase = Math.PI / 3;
-    };
-    draw(camera, locX, locY) {
-        // camera.ctx.beginPath();
-        // camera.ctx.fillStyle = 'red'
-        // camera.ctx.arc(locX, locY, 50, 0, Math.PI / 180, true);
-        // camera.ctx.fill();
-        // console.log('HAHA 😎')
-        let r = this.diameter / 2;
-        camera.ctx.save();
-        camera.ctx.translate(locX, locY);
+  constructor(tank, diameter) {
+    this.tank = tank;
+    this.diameter = diameter;
+    this.phase = Math.PI / 3;
+  }
+  draw(camera, locX, locY) {
+    // camera.ctx.beginPath();
+    // camera.ctx.fillStyle = 'red'
+    // camera.ctx.arc(locX, locY, 50, 0, Math.PI / 180, true);
+    // camera.ctx.fill();
+    // console.log('HAHA 😎')
+    const tankX = this.tank.x;
+    const tankY = this.tank.y;
+    let r = this.diameter / 2;
+    camera.ctx.save();
+    camera.ctx.translate(locX, locY);
 
+    camera.ctx.beginPath();
+    camera.ctx.moveTo(0, 0);
+    camera.ctx.fillStyle = "black";
+    camera.ctx.arc(0, 0, 0.8 * r, 0, 2 * Math.PI, true);
+    camera.ctx.fill();
+    // camera.ctx.rotate(direction)
+
+    camera.ctx.lineWidth = 2;
+    camera.ctx.strokeStyle = "rgba(0, 255, 0, 0.7)"; // Semi-transparent green
+    camera.ctx.moveTo(-r, 0);
+    camera.ctx.lineTo(r, 0);
+    camera.ctx.stroke();
+
+    camera.ctx.moveTo(0, 0 - r);
+    camera.ctx.lineTo(0, r);
+    camera.ctx.stroke();
+
+    camera.ctx.restore();
+
+    this.drawLine(camera, locX, locY);
+
+    const ol = this.getOponentsLocation();
+
+    const orl = this.getOponentsRadarLocation(ol, tankX, tankY);
+    orl.forEach((el) => {
+      if (el.angle > ((360 - 45) * Math.PI) / 180) {
+        el.angle -= 2 * Math.PI;
+      }
+      let tr = 1 - (this.phase - el.angle) / ((45 * Math.PI) / 180);
+      tr = tr < 0 ? 0 : tr;
+      // console.log(
+      //   `phase: ${(this.phase * 180) / Math.PI}, angle: ${
+      //     (el.angle * 180) / Math.PI
+      //   }, tr: ${tr}`
+      // );
+      if (el.angle < this.phase && tr > 0) {
+        camera.ctx.fillStyle = `rgba(100, 100, 255, ${tr})`;
         camera.ctx.beginPath();
-        camera.ctx.moveTo(0, 0);
-        camera.ctx.fillStyle = 'black'
-        camera.ctx.arc(0, 0, 0.8 * r, 0, 2 * Math.PI, true);
+        camera.ctx.arc(locX + el.dx, locY + el.dy, 3, 0, Math.PI * 2);
         camera.ctx.fill();
-        // camera.ctx.rotate(direction)
+      }
+    });
+  }
 
-        camera.ctx.lineWidth = 2;
-        camera.ctx.strokeStyle = 'rgba(0, 255, 0, 0.7)'; // Semi-transparent green
-        camera.ctx.moveTo(- r, 0);
-        camera.ctx.lineTo(r, 0);
-        camera.ctx.stroke();
+  drawLine(camera, locX, locY) {
+    let r = this.diameter / 2;
 
-        camera.ctx.moveTo(0, 0 - r);
-        camera.ctx.lineTo(0, r);
-        camera.ctx.stroke();
+    // camera.ctx.strokeStyle = 'red';
+    camera.ctx.lineWidth = 1;
 
-        camera.ctx.restore();
-
-        this.drawLine(camera, locX, locY);
+    for (let i = 0; i < 45; i++) {
+      camera.ctx.save();
+      camera.ctx.translate(locX, locY);
+      camera.ctx.beginPath();
+      camera.ctx.moveTo(0, 0);
+      camera.ctx.rotate(this.phase - (i * Math.PI) / 180);
+      camera.ctx.strokeStyle = `rgba(0, 255, 0, ${1 - i / 90})`;
+      camera.ctx.lineTo(r * 0.8, 0);
+      camera.ctx.stroke();
+      camera.ctx.restore();
     }
+  }
 
-    drawLine(camera, locX, locY) {
-        let r = this.diameter / 2;
+  update() {
+    this.phase = this.phase + 0.03;
 
-        // camera.ctx.strokeStyle = 'red';
-        camera.ctx.lineWidth = 1;
-
-        for (let i = 0; i < 90; i++) {
-            camera.ctx.save();
-            camera.ctx.translate(locX, locY);
-            camera.ctx.beginPath();
-            camera.ctx.moveTo(0, 0);
-            camera.ctx.rotate(this.phase - i * Math.PI / 180);
-            camera.ctx.strokeStyle = `rgba(0, 255, 0, ${1 - i / 90})`;
-            camera.ctx.lineTo(0, r * 0.8);
-            camera.ctx.stroke();
-            camera.ctx.restore();
-        }
-
+    if (this.phase > 2 * Math.PI) {
+      this.phase = this.phase - 2 * Math.PI;
     }
+  }
 
-    update() {
-        this.phase = this.phase + 0.03;
+  getOponentsLocation() {
+    return Game.players
+      .filter((e) => {
+        return e !== this.tank;
+      })
+      .map((p) => {
+        return {
+          x: p.x,
+          y: p.y,
+        };
+      });
+  }
 
-        if (this.phase > 2 * Math.PI) {
-            this.phase = this.phase - 2 * Math.PI;
-        }
-    }
-};
+  getOponentsRadarLocation(locations, tankX, tankY) {
+    return locations.map((l) => {
+      let dx = l.x - tankX;
+      let dy = l.y - tankY;
+      let angle = Math.atan2(dy, dx);
+
+      dx = (dx * 60) / 5000;
+      dy = (dy * 60) / 5000;
+
+      if (angle < 0) {
+        angle += Math.PI * 2;
+      }
+
+      return {
+        dx: dx,
+        dy: dy,
+        angle: angle,
+      };
+    });
+  }
+}
 
 export { Radar };
--- solider.js
+++ solider.js
@@ -6,352 +6,345 @@ import { UiObjects } from "./arrayuiobjects.js";
 import { LandMine } from "./landMine.js";
 import { Radar } from "./radar.js";
 
-
 class Solider extends UiObject {
+  static {
+    this.audioMovingSrc = UiObject.loadAudio("tank-moving1.mp3");
 
+    this.audioTurretRotatingSrc = UiObject.loadAudio("tank-turret-rotate.mp3");
 
+    this.audioReloadingSrc = UiObject.loadAudio("tank-reload.mp3");
 
-    static {
-        this.audioMovingSrc = UiObject.loadAudio('tank-moving1.mp3')
+    this.audioEmptyGunShotSrc = UiObject.loadAudio("empty-gun-shot.mp3");
 
-        this.audioTurretRotatingSrc = UiObject.loadAudio('tank-turret-rotate.mp3');
+    this.audioTankExplodeSrc = UiObject.loadAudio("tank-explode.mp3");
 
-        this.audioReloadingSrc = UiObject.loadAudio('tank-reload.mp3');
+    this.audioCoolingDownSrc = UiObject.loadAudio("tank-cooling-down.mp3");
+  }
 
-        this.audioEmptyGunShotSrc = UiObject.loadAudio('empty-gun-shot.mp3');
+  constructor(game, x, y, width, height, direction, speed, gunDirection, hp) {
+    super(game, x, y, width, height, hp);
+    this.direction = direction;
+    this.speed = speed;
+    // this.gunDirection = gunDirection;
+    this.image = new Image();
+    this.image.src = "tank1.png";
+    this.gunImage = new Image();
+    this.gunImage.src = "turret.png";
+    this.movingFoward = false;
+    this.rotatingRight = false;
+    this.movingBack = false;
+    this.rotatingLeft = false;
+    this.bgCtx = game.bgctx;
+    this.tracksImg = new Image();
+    this.tracksImg.src = "tracks.png";
+    this.gunDirection = 0;
+    this.turretMovingRight = false;
+    this.turretMovingLeft = false;
+    this.turretSpeed = 0.03;
+    this.bulletsLoaded = 5;
+    this.speedBoost = false;
+    this.speedBoostCouter = 200;
+    this.coolingDown = false;
+    this.explodingSequence = 0;
+    this.exploding = false;
+    this.focusMode = false;
+    this.playerDead = false;
+    // Audios :
+    this.audioMoving = new Audio();
+    this.audioMoving.src = Solider.audioMovingSrc.src;
+    this.audioTurretRotating = new Audio();
+    this.audioTurretRotating.src = Solider.audioTurretRotatingSrc.src;
+    this.audioReloading = new Audio();
+    this.audioReloading.src = Solider.audioReloadingSrc.src;
+    this.audioEmptyGunShot = new Audio();
+    this.audioEmptyGunShot.src = Solider.audioEmptyGunShotSrc.src;
+    this.audioTankExplode = new Audio();
+    this.audioTankExplode.src = Solider.audioTankExplodeSrc.src;
+    this.audioCoolingDown = new Audio();
+    this.audioCoolingDown.src = Solider.audioCoolingDownSrc.src;
+    // Mine indicator
+    this.mineDeployed = false;
+    // Radar indicator
+    this.radarOn = false;
+    // Radar instance
+    this.radar = new Radar(this, 120);
+  }
 
-        this.audioTankExplodeSrc = UiObject.loadAudio('tank-explode.mp3');
+  collisionBox() {
+    return {
+      x: this.x - this.width / 2,
+      y: this.y - this.height / 2,
+      w: this.width,
+      h: this.height,
+    };
+  }
 
-        this.audioCoolingDownSrc = UiObject.loadAudio('tank-cooling-down.mp3');
+  center() {
+    return {
+      x: this.x,
+      y: this.y,
+    };
+  }
+
+  draw(camera) {
+    let co = this.localCoords(camera);
+
+    const center = this.center();
+    camera.ctx.save();
+    camera.ctx.translate(co.x, co.y);
+    camera.ctx.rotate(this.direction);
+    camera.ctx.drawImage(
+      this.image,
+      -this.width / 2,
+      -this.height / 2,
+      this.width,
+      this.height
+    );
+    camera.ctx.restore();
+
+    camera.ctx.save();
+    camera.ctx.translate(co.x, co.y);
+    camera.ctx.rotate(this.direction + this.gunDirection);
+    camera.ctx.drawImage(
+      this.gunImage,
+      -this.width / 2,
+      -this.height / 2,
+      this.width,
+      this.height
+    );
+    camera.ctx.restore();
+
+    if (this.radarOn) {
+      this.radar.draw(camera, co.x, co.y);
     }
 
-    constructor(game, x, y, width, height, direction, speed, gunDirection, hp) {
-        super(game, x, y, width, height, hp)
-        this.direction = direction;
-        this.speed = speed;
-        // this.gunDirection = gunDirection;
-        this.image = new Image()
-        this.image.src = 'tank1.png';
-        this.gunImage = new Image();
-        this.gunImage.src = 'turret.png';
-        this.movingFoward = false;
-        this.rotatingRight = false;
-        this.movingBack = false;
-        this.rotatingLeft = false;
-        this.bgCtx = game.bgctx;
-        this.tracksImg = new Image();
-        this.tracksImg.src = 'tracks.png'
-        this.gunDirection = 0;
-        this.turretMovingRight = false;
-        this.turretMovingLeft = false;
-        this.turretSpeed = 0.03;
+    //Draw a debug geometry: collision box, direction and aim direction.
+    if (Config.debug) {
+      let cbx = this.collisionBox();
+      camera.ctx.strokeStyle = "green";
+      camera.ctx.strokeRect(cbx.x - camera.x, cbx.y - camera.y, cbx.w, cbx.h);
+
+      let px = Math.cos(this.direction) * 50;
+      let py = Math.sin(this.direction) * 50;
+
+      camera.ctx.beginPath();
+      camera.ctx.strokeStyle = "blue";
+      camera.ctx.moveTo(co.x, co.y);
+      camera.ctx.lineTo(co.x + px, co.y + py);
+      camera.ctx.stroke();
+
+      let gx = Math.cos(this.direction + this.gunDirection) * 70;
+      let gy = Math.sin(this.direction + this.gunDirection) * 70;
+
+      camera.ctx.beginPath();
+      camera.ctx.strokeStyle = "red";
+      camera.ctx.moveTo(co.x, co.y);
+      camera.ctx.lineTo(co.x + gx, co.y + gy);
+      camera.ctx.stroke();
+    }
+
+    if (this.exploding === true) {
+      this.drawExplosion(camera);
+    }
+  }
+
+  drawTracks(x, y) {
+    const center = this.center();
+    this.bgCtx.save();
+    this.bgCtx.translate(this.x, this.y);
+    this.bgCtx.rotate(this.direction);
+    this.bgCtx.drawImage(
+      this.tracksImg,
+      -this.width / 2,
+      -this.height / 2,
+      this.width - 15,
+      this.height
+    );
+    this.bgCtx.restore();
+  }
+
+  moveFront(dir = 1) {
+    if (this.radarOn) {
+      return;
+    }
+
+    const oldX = this.x;
+    const oldY = this.y;
+
+    let speed = this.speed;
+
+    if (this.speedBoost) {
+      speed += 1;
+      this.speedBoostCouter--;
+    }
+
+    if (this.speedBoostCouter <= 0) {
+      speed = 0;
+      this.audioCoolingDown.play();
+
+      if (this.coolingDown === false) {
+        setTimeout(() => {
+          this.speedBoostCouter = 200;
+          this.coolingDown = false;
+          this.audioMoving.pause();
+          this.audioCoolingDown.pause();
+        }, 2000);
+      }
+
+      this.coolingDown = true;
+    }
+
+    this.x += dir * Math.cos(this.direction) * speed;
+    this.y += dir * Math.sin(this.direction) * speed;
+
+    let collide = false;
+    UiObjects.forEach((uiobject) => {
+      if (uiobject === this) return;
+      if (this.collides(uiobject)) {
+        collide = true;
+      }
+    });
+    if (collide === true) {
+      this.x = oldX;
+      this.y = oldY;
+      console.log("collide");
+    }
+
+    // Draw tracks of tank
+    this.drawTracks(oldX, oldY);
+  }
+
+  moveBack() {
+    this.moveFront(-1);
+  }
+
+  deployMine() {
+    let myMine = new LandMine(this.game, this.x, this.y, 20, 20, 10, 100, this);
+    this.mineDeployed = false;
+    let nevim = UiObjects.unshift(myMine);
+    console.log(nevim);
+  }
+
+  fire() {
+    if (this.playerDead) {
+      return;
+    }
+    if (this.bulletsLoaded < 1) {
+      this.audioEmptyGunShot.play();
+      return;
+    }
+
+    let myBullet = new Bullet(
+      this.game,
+      this.x,
+      this.y,
+      8,
+      8,
+      5,
+      this.direction + this.gunDirection,
+      20,
+      this
+    );
+
+    this.bulletsLoaded--;
+
+    if (this.bulletsLoaded < 1) {
+      setTimeout(() => {
+        this.audioReloading.play();
         this.bulletsLoaded = 5;
-        this.speedBoost = false;
-        this.speedBoostCouter = 200;
-        this.coolingDown = false;
-        this.explodingSequence = 0;
+      }, 3000);
+    }
+
+    UiObjects.push(myBullet);
+  }
+
+  drawExplosion(camera) {
+    let lc = this.localCoords(camera);
+    let explodeImg = new Image();
+    explodeImg.src = `./Explode-sequence/explode-sequence${this.explodingSequence}.png`;
+    camera.ctx.drawImage(
+      explodeImg,
+      lc.x - explodeImg.width / 2,
+      lc.y - explodeImg.height / 2
+    );
+  }
+
+  explode() {
+    if (this.exploding) return;
+
+    this.exploding = true;
+    this.playerDead = true;
+    this.audioTankExplode.play();
+    let intId = setInterval(() => {
+      this.explodingSequence++;
+      if (this.explodingSequence > 8) {
+        clearInterval(intId);
         this.exploding = false;
-        this.focusMode = false;
-        this.playerDead = false;
-        // Audios :
-        this.audioMoving = new Audio();
-        this.audioMoving.src = Solider.audioMovingSrc.src;
-        this.audioTurretRotating = new Audio();
-        this.audioTurretRotating.src = Solider.audioTurretRotatingSrc.src;
-        this.audioReloading = new Audio();
-        this.audioReloading.src = Solider.audioReloadingSrc.src;
-        this.audioEmptyGunShot = new Audio();
-        this.audioEmptyGunShot.src = Solider.audioEmptyGunShotSrc.src;
-        this.audioTankExplode = new Audio();
-        this.audioTankExplode.src = Solider.audioTankExplodeSrc.src;
-        this.audioCoolingDown = new Audio();
-        this.audioCoolingDown.src = Solider.audioCoolingDownSrc.src;
-        // Mine indicator
-        this.mineDeployed = false;
-        // Radar indicator
-        this.radarOn = false;
-        // Radar instance
-        this.radar = new Radar(120);
+        super.explode();
+      }
+    }, 80);
+  }
+
+  collides(uiobject) {
+    if (uiobject.type === "landMine") {
+      return false;
+    }
+    return super.collides(uiobject);
+  }
+
+  update() {
+    // let dx = this.x - mouse.x;
+    // let dy = this.y - mouse.y;
+    // this.gunDirection = Math.atan2(-dy, -dx);
+    if (this.mineDeployed === true) {
+      this.deployMine();
     }
 
-    collisionBox() {
-        return {
-            x: this.x - this.width / 2,
-            y: this.y - this.height / 2,
-            w: this.width,
-            h: this.height
-        }
+    if (this.movingFoward === true) {
+      this.moveFront();
+    }
+    if (this.movingBack === true) {
+      this.moveBack();
+    }
+    if (this.rotatingRight === true) {
+      this.direction += (this.speed * Math.PI) / 120;
+    }
+    if (this.rotatingLeft === true) {
+      this.direction -= (this.speed * Math.PI) / 120;
+    }
+    let ts = this.turretSpeed;
+
+    if (this.focusMode === true) {
+      ts = ts * 0.3;
+    }
+    if (this.turretMovingLeft) {
+      this.gunDirection -= ts;
+    }
+    if (this.turretMovingRight) {
+      this.gunDirection += ts;
     }
 
-    center() {
-        return {
-            x: this.x,
-            y: this.y
-        }
+    if (this.turretMovingLeft || this.turretMovingRight) {
+      this.audioTurretRotating.play();
+    } else {
+      this.audioTurretRotating.pause();
     }
 
-    draw(camera) {
-
-        let co = this.localCoords(camera);
-
-        const center = this.center()
-        camera.ctx.save()
-        camera.ctx.translate(co.x, co.y)
-        camera.ctx.rotate(this.direction)
-        camera.ctx.drawImage(this.image, -this.width / 2, -this.height / 2, this.width, this.height)
-        camera.ctx.restore()
-
-
-        camera.ctx.save()
-        camera.ctx.translate(co.x, co.y)
-        camera.ctx.rotate(this.direction + this.gunDirection)
-        camera.ctx.drawImage(
-            this.gunImage,
-            -this.width / 2,
-            -this.height / 2,
-            this.width,
-            this.height,
-
-        )
-        camera.ctx.restore()
-
-        if (this.radarOn) {
-            this.radar.draw(camera, co.x, co.y);
-        }
-
-        //Draw a debug geometry: collision box, direction and aim direction.
-        if (Config.debug) {
-            let cbx = this.collisionBox()
-            camera.ctx.strokeStyle = 'green'
-            camera.ctx.strokeRect(cbx.x - camera.x, cbx.y - camera.y, cbx.w, cbx.h)
-
-            let px = Math.cos(this.direction) * 50;
-            let py = Math.sin(this.direction) * 50;
-
-            camera.ctx.beginPath()
-            camera.ctx.strokeStyle = 'blue'
-            camera.ctx.moveTo(co.x, co.y)
-            camera.ctx.lineTo(co.x + px, co.y + py)
-            camera.ctx.stroke()
-
-            let gx = Math.cos(this.direction + this.gunDirection) * 70;
-            let gy = Math.sin(this.direction + this.gunDirection) * 70;
-
-            camera.ctx.beginPath()
-            camera.ctx.strokeStyle = 'red'
-            camera.ctx.moveTo(co.x, co.y)
-            camera.ctx.lineTo(co.x + gx, co.y + gy)
-            camera.ctx.stroke()
-        }
-
-        if (this.exploding === true) {
-            this.drawExplosion(camera)
-        }
+    if (
+      this.movingFoward ||
+      this.movingBack ||
+      this.rotatingLeft ||
+      this.rotatingRight
+    ) {
+      this.audioMoving.play();
+    } else {
+      this.audioMoving.pause();
     }
 
-    drawTracks(x, y) {
+    super.update();
 
-        const center = this.center()
-        this.bgCtx.save()
-        this.bgCtx.translate(this.x, this.y)
-        this.bgCtx.rotate(this.direction)
-        this.bgCtx.drawImage(this.tracksImg,
-            -this.width / 2,
-            -this.height / 2,
-            this.width - 15,
-            this.height)
-        this.bgCtx.restore()
-    }
-
-    moveFront(dir = 1) {
-
-        if (this.radarOn) {
-            return;
-        }
-
-        const oldX = this.x
-        const oldY = this.y
-
-        let speed = this.speed;
-
-        if (this.speedBoost) {
-            speed += 1;
-            this.speedBoostCouter--;
-        }
-
-        if (this.speedBoostCouter <= 0) {
-            speed = 0;
-            this.audioCoolingDown.play();
-
-            if (this.coolingDown === false) {
-                setTimeout(
-                    () => {
-                        this.speedBoostCouter = 200;
-                        this.coolingDown = false;
-                        this.audioMoving.pause();
-                        this.audioCoolingDown.pause();
-                    }, 2000
-                )
-            }
-
-            this.coolingDown = true;
-        }
-
-        this.x += dir * Math.cos(this.direction) * speed;
-        this.y += dir * Math.sin(this.direction) * speed;
-
-        let collide = false;
-        UiObjects.forEach(
-            (uiobject) => {
-                if (uiobject === this) return;
-                if (this.collides(uiobject)) {
-                    collide = true;
-                }
-            }
-        )
-        if (collide === true) {
-            this.x = oldX
-            this.y = oldY
-            console.log('collide')
-        }
-
-        // Draw tracks of tank
-        this.drawTracks(oldX, oldY);
-    }
-
-    moveBack() {
-        this.moveFront(-1);
-    }
-
-    deployMine() {
-        let myMine = new LandMine(this.game, this.x, this.y, 20, 20, 10, 100, this);
-        this.mineDeployed = false;
-        let nevim = UiObjects.unshift(myMine)
-        console.log(nevim)
-    }
-
-    fire() {
-        if (this.playerDead) {
-            return;
-        }
-        if (this.bulletsLoaded < 1) {
-            this.audioEmptyGunShot.play();
-            return;
-        }
-
-
-        let myBullet = new Bullet(
-            this.game,
-            this.x,
-            this.y,
-            8,
-            8,
-            5,
-            this.direction + this.gunDirection,
-            20,
-            this
-        );
-
-        this.bulletsLoaded--;
-
-        if (this.bulletsLoaded < 1) {
-            setTimeout(
-                () => {
-                    this.audioReloading.play();
-                    this.bulletsLoaded = 5;
-                }, 3000
-            )
-        }
-
-        UiObjects.push(myBullet)
-    }
-
-    drawExplosion(camera) {
-        let lc = this.localCoords(camera);
-        let explodeImg = new Image();
-        explodeImg.src = `./Explode-sequence/explode-sequence${this.explodingSequence}.png`
-        camera.ctx.drawImage(
-            explodeImg,
-            lc.x - explodeImg.width / 2,
-            lc.y - explodeImg.height / 2
-        )
-    }
-
-    explode() {
-        if (this.exploding) return;
-
-        this.exploding = true;
-        this.playerDead = true;
-        this.audioTankExplode.play();
-        let intId = setInterval(() => {
-            this.explodingSequence++;
-            if (this.explodingSequence > 8) {
-                clearInterval(intId);
-                this.exploding = false;
-                super.explode();
-            }
-        }, 80);
-    }
-
-    collides(uiobject) {
-        if (uiobject.type === 'landMine') {
-            return false;
-        }
-        return super.collides(uiobject);
-    }
-
-    update() {
-        // let dx = this.x - mouse.x;
-        // let dy = this.y - mouse.y;
-        // this.gunDirection = Math.atan2(-dy, -dx);
-        if (this.mineDeployed === true) {
-            this.deployMine();
-        }
-
-        if (this.movingFoward === true) {
-            this.moveFront();
-        }
-        if (this.movingBack === true) {
-            this.moveBack();
-        }
-        if (this.rotatingRight === true) {
-            this.direction += this.speed * Math.PI / 120;
-        }
-        if (this.rotatingLeft === true) {
-            this.direction -= this.speed * Math.PI / 120;
-        }
-        let ts = this.turretSpeed;
-
-        if (this.focusMode === true) {
-            ts = ts * 0.3;
-        }
-        if (this.turretMovingLeft) {
-            this.gunDirection -= ts;
-        }
-        if (this.turretMovingRight) {
-            this.gunDirection += ts;
-        }
-
-        if ((this.turretMovingLeft) || (this.turretMovingRight)) {
-            this.audioTurretRotating.play();
-        } else {
-            this.audioTurretRotating.pause();
-        }
-
-        if ((this.movingFoward) ||
-            (this.movingBack) ||
-            (this.rotatingLeft) ||
-            (this.rotatingRight)) {
-            this.audioMoving.play();
-        } else {
-            this.audioMoving.pause();
-        }
-
-        super.update();
-
-        this.radar.update();
-    }
-
+    this.radar.update();
+  }
 }
 
-export { Solider }
+export { Solider };
