ae9e778d534cdeb269740fbb67c2034b5f60a1a5
Adam Bures <adam.bures.prg@gmail.com>
2024-01-07 11:10:21 +0000

    Created join leave room

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import { dirname, join } from "node:path";
 import { Server } from "socket.io";
 import { ServerGame } from "./serverGame.js";
 import { Player } from "./player.js";
+import { Room } from "./room.js";
 
 const serverGame = new ServerGame();
 const app = express();
@@ -21,16 +22,44 @@ io.on("connection", (socket) => {
   socket.on("chat message", (msg) => {
     io.emit("chat message", msg);
   });
-  if (!serverGame.hasPlayer(socket.id)) {
-    let p = new Player(socket);
-    serverGame.players.push(p);
-  }
-  console.log(serverGame);
+
+  serverGame.acceptOrUpdateConnection(socket);
+
+  socket.on("join-room", (msg) => {
+    console.log(`Joining room: ${msg}`);
+    socket.join(msg);
+
+    let r = serverGame.getOrCreateRoomById(msg);
+    let p = serverGame.getOrCreatePlayer(socket);
+
+    r.playerJoinRoom(p);
+    console.log(getInfo());
+  });
+
+  socket.on("leave-room", (msg) => {
+    socket.leave(msg);
+    let r = serverGame.getOrCreateRoomById(msg);
+    r.removePlayer(socket);
+    console.log(getInfo());
+  });
 
   socket.on("disconnect", () => {
-    serverGame.removePlayer(socket.id);
+    serverGame.removePlayer(socket);
+    console.log(getInfo());
   });
+
+  console.log(getInfo());
 });
+
+function getInfo() {
+  let str = `Game has : ${serverGame.players.length} players: `;
+  serverGame.players.forEach((p) => (str += `${p.socket.id},`));
+  str += "\n  Rooms:";
+  serverGame.rooms.forEach(
+    (r) => (str += `  ${r.id} (${r.players.length} players)\n`)
+  );
+  return str;
+}
 
 server.listen(3001, () => {
   console.log("server running at http://localhost:3001");
---
+++ server/room.js
@@ -0,0 +1,57 @@
+import { Player } from "./player.js";
+
+/**
+ * @typedef { Object } Room
+ * @property {String} id - An ID of a room, can be any string.
+ * @property {Array<Player>} players - A list of Players that joined the room.
+ */
+
+/**
+ * Represents a room (one world where a fight of players happens)
+ *
+ */
+class Room {
+  /**
+   * @constructor
+   * @param {String} id - An ID of a room, can be any string.
+   */
+  constructor(id) {
+    this.id = id;
+    this.players = [];
+  }
+
+  /**
+   * Adds a player to the room if does not exist yet.
+   * @param {Player} player to join the room.
+   * @returns {boolean} true if player was added, false if the player already is added.
+   */
+  playerJoinRoom(player) {
+    if (!this.getPlayer(player.socket)) {
+      this.players.push(player);
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * Returns a player in the room, based on the socket.
+   *
+   * @param {socket.io.socket} socket
+   * @returns {Player | null} - Returns a player if exists in the room, null otherwise.
+   */
+  getPlayer(socket) {
+    const pls = this.players.filter((p) => p.socket.id === socket.id);
+    if (pls.length === 0) return null;
+    return pls[0];
+  }
+
+  /**
+   * Removes a player from a room, based on his socket.
+   * @param {socket.io.socket} socket of a player to be removed.
+   */
+  removePlayer(socket) {
+    this.players = this.players.filter((p) => p.socket.id !== socket.id);
+  }
+}
+
+export { Room };
