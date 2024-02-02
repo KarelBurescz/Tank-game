c62222f8e1b34953e9b90416f00d58f5c11c3e4f
Pavel <pavel.bures@gmail.com>
2024-02-02 19:30:58 +0000

    drawing the visibility rays

--- model/geometry.js
+++ model/geometry.js
@@ -2,7 +2,10 @@ class Geometry {
   constructor() {
 
   }
-  static linesIntersection(x1, y1, x2, y2, x3, y3, x4, y4) {
+  static linesIntersection(
+    x1, y1, x2, y2, x3, y3, x4, y4,
+    p1end = false, p2end = false, p3end = false, p4end = false
+    ) {
     const A1 = y1 - y2;
     const A3 = y3 - y4;
     const B1 = x2 - x1;
@@ -18,8 +21,37 @@ class Geometry {
 
     const x = (C1 * B3 - C3 * B1) / det;
     const y = (C3 * A1 - C1 * A3) / det;
+    
+    const tests = [
+      [p1end, x1, y1, x, y, x2, y2],
+      [p2end, x2, y2, x, y, x1, y1],
+      [p3end, x3, y3, x, y, x4, y4],
+      [p4end, x4, y4, x, y, x3, y3],
+    ];
 
+    const mt = tests.map( (t) => {
+      let [b, ...pts] = t;
+      return b?this._testBoundaryPoint(...pts):true;
+    })
+
+    if (mt.some(e => e === false)){
+      return undefined;
+    }
+      
     return [x, y];
+  }
+
+  /**
+   * Tests if an intersection (xi, yi) lays on a half line, 
+   * stat starts in (x1, y1) and goes through (x2, y2) to
+   * infinity. 
+   * The function assumes that (xi,yi) lays on the line going
+   * through (x1,y1) and (x2,y2).
+   */
+  static _testBoundaryPoint(x1,y1, xi, yi, x2, y2){
+    let resx = (x1 <= x2) ? (x1 <= xi) : (x1 >= xi);
+    let resy = (y1 <= y2) ? (y1 <= yi) : (y1 >= yi);
+    return resx && resy;
   }
 }
 
--- model/test/geometry_test.js
+++ model/test/geometry_test.js
@@ -50,5 +50,79 @@ describe('model/geometry', () => {
       )
       expect(result).to.be(undefined);
     });
+
+    it('should not find intersection if the lines are limited by their bounding points', () => {
+      let result = Geometry.linesIntersection(
+        0, 0,
+        2, 1,
+        0, 2,
+        6, 2,
+        false,
+        true,
+        false,
+        false
+      );
+
+      expect(result).to.be(undefined);
+
+    });
+    it('should find intersection if the lines are not limited by their bounding points', () => {
+      let result = Geometry.linesIntersection(
+        0, 0,
+        2, 1,
+        0, 2,
+        6, 2,
+        false,
+        false,
+        false,
+        false
+      );
+
+      expect(result).to.eql([4,2]);
+    });
+    it('should not find intersection of limited semi lines parallel to x or y axes', () => {
+      let result = Geometry.linesIntersection(
+        0, 0,
+        2, 0,
+        3, -1,
+        3, 3,
+        false,
+        true,
+        false,
+        false
+      );
+
+      expect(result).to.be(undefined);
+    });
+
+    it('should find intersection of limited semi lines if they share a single point', () => {
+      let result = Geometry.linesIntersection(
+        0, 0,
+        4, 4,
+        1, 1,
+        3, 0,
+        false,
+        false,
+        true,
+        false
+      );
+
+      expect(result).to.eql([1,1]);
+    });
+
+    it('should find intersection of limited semi lines if they both start from the same point', () => {
+      let result = Geometry.linesIntersection(
+        3, 2,
+        4, 4,
+        3, 2,
+        1, -4,
+        true,
+        true,
+        true,
+        true
+      );
+
+      expect(result).to.eql([3,2]);
+    });
   });
 });
